80a000c0814a4904e0c8f7c9380eef582fc00a16
Coderfred1010 <frederik.fuglo@gmail.com>
2025-08-29 10:57:03 +0200

    Complteted Exercise 01

--- src/main/java/ek/osnb/jpa/orders/model/Order.java
+++ src/main/java/ek/osnb/jpa/orders/model/Order.java
@@ -1,11 +1,19 @@
 package ek.osnb.jpa.orders.model;
 
-import java.time.LocalDate;
+import ek.osnb.jpa.common.model.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
 
-public class Order {
+import java.time.LocalDate;
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity {
 
     private LocalDate orderDate;
 
+    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
     public Order() {}
